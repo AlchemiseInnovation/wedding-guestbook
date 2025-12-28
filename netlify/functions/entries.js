9585f5b240ab981b77a2132257e260788b03514a
AlchemiseInnovation <aiclothingbytsm@gmail.com>
2025-12-28 11:44:54 +0000

    Update entries.js

--- netlify/functions/entries.js
+++ netlify/functions/entries.js
@@ -1,10 +1,6 @@
-export const handler = async (event, context) => {
-  const blob = await context.blob.get("entries");
-
-  const entries = blob ? JSON.parse(blob) : [];
-
+export const handler = async () => {
   return {
     statusCode: 200,
-    body: JSON.stringify(entries)
+    body: JSON.stringify([])
   };
 };
